c7a9a5e06f637106f72c3b37915a130bb8e06157
openrtc <isao-hara@aist.go.jp>
2018-05-25 09:25:13 +0900

    Update 2018.5.25-1

--- NameServer.py
+++ NameServer.py
@@ -7,19 +7,14 @@ from __future__ import print_function
 from NameService import *
 import NameService.xmlrpc as xmlrpc
 
-name_server = None
-
 def initNS():
-    global name_server
-    if name_server is None:
-        name_server = NameService()
-        return "OK"
-    return "Already running..."
+    return xmlrpc.initNS()
 
 def main():
-    global name_server
-    name_server = NameService()
-    name_server.run()
+    xmlrpc.initNS()
+    xmlrpc._setup()
+    xmlrpc._start()
+    #xmlrpc.name_server.run()
 
 if __name__ == '__main__':
     main()
